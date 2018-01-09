579d2e943eaf11baf6874d0dfea3686fb0501fb1
shwoodley1 <32753315+shwoodley1@users.noreply.github.com>
2018-01-09 12:07:11 -0400

    mutation

--- Scratch_pad.js
+++ Scratch_pad.js
@@ -1,3 +1,15 @@
+function mutation(arr) {
+arr1 = arr[0].toLowerCase().split("").sort();
+arr2 = arr[1].toLowerCase().split("").sort();
+for (var i = 0; i < arr2.length; i++){
+    if (arr1.indexOf(arr2[i])===-1)
+        console.log(false);
+}
+   console.log (true);
+     }
+mutation(["Mary", "mmatry"]);
+
+
 function chunkArrayInGroups(arr, size) {
  answer = [];
    for (var i = 0; 0 < arr.length; i++){
