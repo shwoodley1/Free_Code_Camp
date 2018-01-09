2de64604b3c8f859c2b17c76c45c64aeb9500033
shwoodley1 <32753315+shwoodley1@users.noreply.github.com>
2018-01-09 11:48:47 -0400

    added Find longest word

--- Scratch_pad.js
+++ Scratch_pad.js
@@ -1,3 +1,9 @@
+findLongestWord("The quick brown fox jumped over the lazy dog");
+
+function findLongestWord(str){
+  document.write(str.split(' ').sort(function (a,b){return b.length - a.length})[0].length); 
+
+
  function palindrome(str){
     shane = str.toLowerCase().replace(/[^A-Za-z0-9_]/g,"");
      answer = (shane === shane.split('').reverse().join(''))
